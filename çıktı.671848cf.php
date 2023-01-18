76f44439c8bdc3f7f92668281861262b267c5237
yunus-TALHA-coban <116153517+yunus-TALHA-coban@users.noreply.github.com>
2023-01-18 16:11:50 +0300

    Add files via upload

---
+++ çıktı.671848cf.php
@@ -0,0 +1,10 @@
+<?php
+    if($_GET){
+        $ad = $_GET["ad"],
+        $soy = $_GET["soy"],
+        $tel = $_GET["tel"],
+        $mektep = $_GET["mektep"],
+        $sinif= $_GET["sinif"],
+        $osm = $_GET["osm"],
+    }
+?>
